a5629147369f7bf6fad768be88ee61f752860990
Shaw Terwilliger <sterwill@tinfig.com>
2018-11-15 17:08:43 -0500

    Relaxed requirements versions

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.4',
+    version='1.5',
 
     description='Tools to manage CloudFormation stack configuration',
     long_description=long_description,
@@ -73,11 +73,11 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'boto3>=1.9',
-        'click>=6.7',
-        'PyYAML>=3.13',
-        'cfn_flip>=1.0.4',
-        'aws-sam-translator>=1.8.0',
+        'boto3~=1.9',
+        'click~=6.7',
+        'PyYAML~=3.0',
+        'cfn_flip~=1.0.4',
+        'aws-sam-translator~=1.8.0',
     ],
 
     # List additional groups of dependencies here (e.g. development
