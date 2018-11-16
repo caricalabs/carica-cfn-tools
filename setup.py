6b1a94901bb9daff60a0f1214cf1418dfdd428fe
Shaw Terwilliger <sterwill@tinfig.com>
2018-11-16 10:03:44 -0500

    Use Click 7, remove comments in setup.py

--- setup.py
+++ setup.py
@@ -19,91 +19,34 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='carica_cfn_tools',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.5',
-
+    version='1.6',
     description='Tools to manage CloudFormation stack configuration',
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/caricalabs/carica-cfn-tools',
-
-    # Author details
     author='Carica Labs, LLC',
     author_email='info@caricalabs.com',
-
-    # Choose your license
     license='APL 2.0',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
         'Intended Audience :: System Administrators',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: Apache Software License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
     ],
-
-    # What does your project relate to?
     keywords='cloudformation cfn stack template config configuration',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'boto3~=1.9',
-        'click~=6.7',
+        'click~=7.0',
         'PyYAML~=3.0',
         'cfn_flip~=1.0.4',
         'aws-sam-translator~=1.8.0',
     ],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
     extras_require={
         'dev': ['check-manifest'],
         'test': [],
     },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
     package_data={
     },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
             'carica-cfn=carica_cfn_tools.cli:cli',
